7c0c7d34c5fe5cc07d8313fbfe9dd5f48fd3fa19
Nymfeparakit <maxlex047@gmail.com>
2020-11-27 18:39:35 +0300

    Add about page

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name='pages/index.html'
+
+
+class AboutView(TemplateView):
+    template_name='pages/about.html'
