e79415628f621026009cdade2834d3415243d3ed
Nymfeparakit <maxlex047@gmail.com>
2020-12-01 12:23:32 +0300

    Add feed and source models

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Feed(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    sources = models.ManyToManyField('Source')
+
+class Source(models.Model):
+    name = models.CharField(max_length=30)
+    icon = models.ImageField(upload_to='sources_icons/')
