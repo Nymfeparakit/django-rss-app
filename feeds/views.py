6ddae4469c72d623bf809c6d3f533c04abe8180c
Nymfeparakit <maxlex047@gmail.com>
2020-12-02 10:34:11 +0300

    Show articles titles in table

--- feeds/views.py
+++ feeds/views.py
@@ -13,12 +13,16 @@ class FeedTodayView(TemplateView):
         # today feed always exist for user
         today_feed = Feed.objects.get(user=self.request.user, name='today') 
         # get all sources in this feed
-        titles = []
+        articles = []
         for source in today_feed.sources.all():
             # get rss data by source's link
-            rss_data = requests.get(source.url).text.encode('utf-8')
+            rss_text = requests.get(source.url).text
+            rss_data = rss_text.encode('utf-8')
+            f = open('rss_data_techcrunch.xml', 'w')
+            f.write(rss_text)
             parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
             root = etree.fromstring(rss_data, parser=parser)
-            titles += root.findall('.//title')
-            context['titles'] = [title.text for title in titles]
+            titles = root.findall('.//item/title')
+            articles += [{'source': source.name, 'title': title.text} for title in titles]
+        context['articles'] = articles
         return context
