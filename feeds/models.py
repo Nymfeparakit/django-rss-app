81658aeaaa7a791bea4810f6a31e062765b9a80d
Nymfeparakit <maxlex047@gmail.com>
2020-12-02 10:00:54 +0300

    Add __str__ methods to models

--- feeds/models.py
+++ feeds/models.py
@@ -6,7 +6,13 @@ class Feed(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     sources = models.ManyToManyField('Source')
 
+    def __str__(self):
+        return self.name
+
 class Source(models.Model):
     name = models.CharField(max_length=30)
     icon = models.ImageField(upload_to='sources_icons/')
     url = models.URLField()
+
+    def __str__(self):
+        return self.name
