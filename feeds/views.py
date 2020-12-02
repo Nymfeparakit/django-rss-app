574cf30759cecb83886ee4b434b686db43b0075b
Nymfeparakit <maxlex047@gmail.com>
2020-12-02 10:01:56 +0300

    Show articles titles for all sources in 'today' feed

--- feeds/views.py
+++ feeds/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import requests
+from lxml import etree
+from .models import Feed, Source
 
 class FeedTodayView(TemplateView):
     template_name='feeds/articles.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get 'today' feed of current user
+        # today feed always exist for user
+        today_feed = Feed.objects.get(user=self.request.user, name='today') 
+        # get all sources in this feed
+        titles = []
+        for source in today_feed.sources.all():
+            # get rss data by source's link
+            rss_data = requests.get(source.url).text.encode('utf-8')
+            parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
+            root = etree.fromstring(rss_data, parser=parser)
+            titles += root.findall('.//title')
+            context['titles'] = [title.text for title in titles]
+        return context
