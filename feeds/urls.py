87009dae207224f7db8bb3ebb2588566ad231aff
Nymfeparakit <maxlex047@gmail.com>
2020-12-03 08:58:54 +0300

    Add url for article detail

--- feeds/urls.py
+++ feeds/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('today/', views.FeedTodayView.as_view(), name='today'),
+    #path('article/<article_title>', views.ArticleDetailView.as_view(), name='article'),
+    path('article/<int:article_id>', views.ArticleDetailView.as_view(), name='article'),
 ]
