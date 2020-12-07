d6f4f5069d4115faff0cc0a950feb308da1871a4
Nymfeparakit <maxlex047@gmail.com>
2020-12-07 11:04:05 +0300

    Add new source to user's feed

--- feeds/views.py
+++ feeds/views.py
@@ -6,6 +6,7 @@ from lxml import etree
 import os, json
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.core.files.base import ContentFile
 
 from .models import Feed, Source
 
@@ -76,7 +77,6 @@ class SourceListView(ListView):
         today_feed = Feed.objects.get(user=self.request.user, name='today') 
         return today_feed.sources.all()
 
-
     def get(self, request, *args, **kwargs):
         print('get request')
         return super().get(request, args, kwargs)
@@ -84,7 +84,7 @@ class SourceListView(ListView):
     def post(self, request, *args, **kwargs):
         source_url = self.request.POST['source_url']
 
-        # get source name and icon from rss data
+        # get source name and icon url from rss data
         rss_data = requests.get(source_url).text.encode('utf-8')
         parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
         root = etree.fromstring(rss_data, parser=parser)
@@ -92,19 +92,21 @@ class SourceListView(ListView):
         source_name = root.find('.//channel/title').text
         icon_url = root.find('.//channel/image/url').text
         icon_title = root.find('.//channel/image/title').text
-        self.download_source_icon(icon_url, icon_title)
-        print('new source name:', source_name)
 
-        #new_source = Source(name='new source', url = 'https://www.rbc.ru/')
-        #new_source.save()
-        #today_feed = Feed.objects.get(user=self.request.user, name='today') 
-        #today_feed.sources.add(new_source)
+        # save new source in 'today' feed
+        new_source = Source(name=source_name, url=source_url)
+        new_source.save()
+        self.download_source_icon(icon_url, icon_title, new_source)
+        today_feed = Feed.objects.get(user=self.request.user, name='today') 
+        today_feed.sources.add(new_source)
+
         return HttpResponseRedirect('')
 
-    def download_source_icon(self, icon_url, icon_title):
-        icon_page = requests.get(icon_url)
+    def download_source_icon(self, icon_url, icon_title, source):
+        icon_response = requests.get(icon_url)
         icon_extension = os.path.splitext(icon_url)[-1]
-        icon_file = f'{icon_title}.{icon_extension}'
-        with open(f'{settings.MEDIA_ROOT}/sources_icons/{icon_file}', 'wb') as f:
-            f.write(icon_page.content)
+        icon_filename = f'{icon_title}.{icon_extension}'
+        source.icon.save(icon_filename, ContentFile(icon_response.content), save=True)
+        #with open(f'{settings.MEDIA_ROOT}/sources_icons/{icon_file}', 'wb') as f:
+        #    f.write(icon_page.content)
 
